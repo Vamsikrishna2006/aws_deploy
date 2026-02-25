6822296818b6dab5c6a16665ecb7a88e53e21c6c
Vamsikrishna2006 <vamsiroy.2006@gmail.com>
2026-02-25 16:10:48 +0530

    updated

--- src/main/java/com/example/demo/cseController.java
+++ src/main/java/com/example/demo/cseController.java
@@ -1,5 +1,19 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class cseController {
+	@GetMapping("/home")
+	public String disk() {
+		return "Welcome to Cse Jenkins from aditya iniversoty";
+	}
+	@GetMapping("/cse")
+	public String disk2() {
+		return "Welcome to Aditya University";
+	}
 
 }
+
+
